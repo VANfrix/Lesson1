5a31ce7edcecc1a61ee6a538848d62240c86353b
VANfrix <ultrafrost2012@yandex.ru>
2022-02-14 15:36:42 +0300

    Проценты

--- hello3.py
+++ hello3.py
@@ -1,4 +1,4 @@
 weather={"city":"Москва", "temerature":"20"}
 print(weather["city"])
-weather["temerature"]=int(weather["temerature"])-5
-print(["temperatire"])
+#weather["temerature"]=int(weather["temerature"])-5
+#print(["temperatire"])
--- hello4.py
+++ hello4.py
@@ -1,1 +1,4 @@
-def dicount
+phones = ["iPhone 12", "Samsung Galaxy S21", "Xiaomi Mi11"]
+print(phones[0:3])
+#phones.append('iPhone 12')
+#print(phones.count('iPhone 12'))
