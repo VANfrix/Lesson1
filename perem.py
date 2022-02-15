6ef00baca1a44d53b87cca857450777a900b3630
VANfrix <ultrafrost2012@yandex.ru>
2022-02-16 01:00:10 +0300

    Начал задание Ppice

--- perem.py
+++ perem.py
@@ -1,29 +1,4 @@
-# def get_summ(x, y, z):
-#     return x + y + z
-# get_summ('one', '&','two')
-
-
-def get_summ(one, two, delimiter='&'):
-    print(f'{one} {delimiter} {two}')
-get_summ("Learn","Python")
-
-# def a_function():
-#     print("You just created a function!")
-# a_function()
-
-# def add(a, b):
-#     return a + b
- 
-# print( add(1, 2) ) # 3
-# print( add(a = 2, b = 3) ) # 5
- 
-# total = add(b = 4, a = 5)
-# print(total) # 9
-
-# def add(x, y):
-#     return x + y
-# add('abc', 'def')
-
-# def func (a, b, c=2):
-#     return a + b + c
-# func(1, 2)
+def get_summ(one='Learn', two='Python', delimiter='&'):
+    summ =(f'{one} {delimiter} {two}')
+    print(summ.upper())
+get_summ ()
